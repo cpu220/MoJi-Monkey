a3cbed5ee136ba308d108a0ba2954f5fe08948e3
Belial <102864814@qq.com>
2023-09-25 19:16:55 +0800

    Site updated: 2023-09-25 19:16:55

---
+++ js/index.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+  Theme.backToTop.register();
+  Theme.showHeadBar.register();
+  Theme.clipboard.register();
+  Theme.navbar.register();
+  Theme.search.register();
+
+  [].forEach.call(document.getElementsByClassName('loading-wrapper'),
+    element => {
+      element.setAttribute('data-loading', 'false');
+    });
+
+  [].forEach.call(document.getElementsByClassName('page'),
+    element => {
+      element.setAttribute('data-filter', 'false');
+    });
+};
